5275652d0a60ef26bd91662f0e2ac75f1dbb6ebd
Nancy <chemutain@gmail.com>
2020-04-08 20:40:24 +0300

    Add and Delete record from the db

--- app/src/main/java/com/example/dataapp/MyDBHandler.java
+++ app/src/main/java/com/example/dataapp/MyDBHandler.java
@@ -1,5 +1,6 @@
 package com.example.dataapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -15,16 +16,32 @@ public class MyDBHandler extends SQLiteOpenHelper {
     private static final String COLUMN_PRODUCT_NAME = "product_name";
 
     public MyDBHandler(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        String query = "CREATE TABLE " + TABLE_PRODUCTS + "(" + COLUMN_ID + "INTEGER PRIMARY KEY AUTO INCREMENT," + COLUMN_PRODUCT_NAME + "TEXT" + ")";
+        sqLiteDatabase.execSQL(query);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_PRODUCTS);
+        onCreate(sqLiteDatabase);
+    }
 
+    public void addProduct(Products products){
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_PRODUCT_NAME, products.getProduct_name());
+        SQLiteDatabase db = getWritableDatabase();
+        db.insert(TABLE_PRODUCTS, null, values);
+        db.close();
+
+    }
+
+    public void deteleProduct(String productname){
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("DELETE FROM " + "TABLE_PRODUCTS" + "WHERE" + COLUMN_PRODUCT_NAME + "=\""+ productname + "\" ; ");
     }
 }
